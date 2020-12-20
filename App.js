bcdc53f457b93cd409132856e33e31be06a6ae04
Kaan ER <kaaner91@gmail.com>
2020-12-20 21:25:52 +0300

    game completed

--- App.js
+++ App.js
@@ -1,4 +1,4 @@
-import React, {useState} from 'react';
+import React, {useState, useEffect} from 'react';
 import Icon from 'react-native-vector-icons/FontAwesome';
 import {CountdownCircleTimer} from 'react-native-countdown-circle-timer';
 
@@ -14,8 +14,18 @@ import {
 } from 'react-native';
 
 const App = () => {
-  const [playerOneClock, setPlayerOneClock] = useState(500);
-  const [playerTwoClock, setPlayerTwoClock] = useState(500);
+  const [playerOneKey, setPlayerOneKey] = useState(0);
+  const [playerTwoKey, setPlayerTwoKey] = useState(0);
+  const [playerOneMoves, setPlayerOneMoves] = useState(0);
+  const [playerTwoMoves, setPlayerTwoMoves] = useState(0);
+  const [playerOneClock, setPlayerOneClock] = useState(5 * 60 * 60);
+  const [playerTwoClock, setPlayerTwoClock] = useState(5 * 60 * 60);
+  const [playerOneRemainingTime, setPlayerOneRemainingTime] = useState(
+    3600 * 5,
+  );
+  const [playerTwoRemainingTime, setPlayerTwoRemainingTime] = useState(
+    3600 * 5,
+  );
   const [isPlayingOne, setIsPlayingForOne] = useState(false);
   const [isPlayingTwo, setIsPlayingForTwo] = useState(false);
   const [isTurnForOne, setIsTurnForOne] = useState(false);
@@ -25,6 +35,17 @@ const App = () => {
     const hours = Math.floor(remainingTime / 3600);
     const minutes = Math.floor((remainingTime % 3600) / 60);
     const seconds = remainingTime % 60;
+
+    useEffect(() => {
+      if (isTurnForOne) {
+        setPlayerOneRemainingTime(remainingTime);
+        setPlayerOneClock(remainingTime);
+      }
+      if (isTurnForTwo) {
+        setPlayerTwoRemainingTime(remainingTime);
+        setPlayerTwoClock(remainingTime);
+      }
+    }, [setPlayerOneRemainingTime, setPlayerTwoRemainingTime, remainingTime]);
 
     return `${hours}:${minutes}:${seconds}`;
   };
@@ -42,10 +63,19 @@ const App = () => {
   const onPressRestart = () => {
     setIsTurnForOne(false);
     setIsTurnForTwo(false);
-    setPlayerOneClock(500);
-    setPlayerTwoClock(500);
-    //setIsPlayingForOne(false);
-    //setIsPlayingForTwo(false);
+
+    setPlayerOneKey(5 * 3600);
+    setPlayerOneRemainingTime(5 * 3600);
+    setPlayerOneClock(5 * 3600);
+    setPlayerOneMoves(0);
+
+    setPlayerTwoKey(5 * 3600);
+    setPlayerTwoRemainingTime(5 * 3600);
+    setPlayerTwoClock(5 * 3600);
+    setPlayerTwoMoves(0);
+
+    setIsPlayingForOne(false);
+    setIsPlayingForTwo(false);
   };
 
   const onPressPlayerOneTimer = () => {
@@ -53,6 +83,9 @@ const App = () => {
     setIsTurnForTwo(true);
     setIsPlayingForOne(false);
     setIsPlayingForTwo(true);
+    setPlayerOneKey(playerOneRemainingTime + 2);
+    setPlayerOneRemainingTime(playerOneRemainingTime + 2);
+    setPlayerOneMoves((moves) => moves + 1);
   };
 
   const onPressPlayerTwoTimer = () => {
@@ -60,6 +93,9 @@ const App = () => {
     setIsTurnForTwo(false);
     setIsPlayingForOne(true);
     setIsPlayingForTwo(false);
+    setPlayerTwoKey(playerTwoRemainingTime + 2);
+    setPlayerTwoRemainingTime(playerTwoRemainingTime + 2);
+    setPlayerTwoMoves((moves) => moves + 1);
   };
 
   return (
@@ -69,16 +105,23 @@ const App = () => {
         <View style={[styles.box1, styles.box]}>
           <TouchableOpacity onPress={onPressPlayerOneTimer}>
             <CountdownCircleTimer
-              // onComplete={() => {
-              //   // do your stuff here
-              //   return [true, 1500]; // repeat animation in 1.5 seconds
-              // }}
+              key={playerOneKey}
+              onComplete={() => {
+                // do your stuff here
+                return [false, 1500]; // repeat animation in 1.5 seconds
+              }}
               isPlaying={isPlayingOne}
               duration={playerOneClock}
-              colors="#A30000">
+              initialRemainingTime={playerOneRemainingTime}
+              colors="#0e0e0e">
               {({remainingTime, animatedColor}) => (
-                <Animated.Text style={{...styles.remainingTime}}>
-                  {children(remainingTime)}
+                <Animated.Text>
+                  <View style={styles.timer}>
+                    <Text>Moves: {playerOneMoves}</Text>
+                    <Text style={{...styles.remainingTime}}>
+                      {children(remainingTime)}
+                    </Text>
+                  </View>
                 </Animated.Text>
               )}
             </CountdownCircleTimer>
@@ -119,18 +162,23 @@ const App = () => {
         <View style={[styles.box2, styles.box]}>
           <TouchableOpacity onPress={onPressPlayerTwoTimer}>
             <CountdownCircleTimer
+              key={playerTwoKey}
               isPlaying={isPlayingTwo}
               duration={playerTwoClock}
-              // initialRemainingTime={600}
-              colors="#A30000"
-              // onComplete={() => {
-              //   // do your stuff here
-              //   return [true, 1500]; // repeat animation in 1.5 seconds
-              // }}
-            >
+              initialRemainingTime={playerTwoRemainingTime}
+              colors="#0e0e0e"
+              onComplete={() => {
+                // do your stuff here
+                return [false, 1500]; // repeat animation in 1.5 seconds
+              }}>
               {({remainingTime, animatedColor}) => (
-                <Animated.Text style={{...styles.remainingTime}}>
-                  {children(remainingTime)}
+                <Animated.Text>
+                  <View style={styles.timer}>
+                    <Text>Moves: {playerTwoMoves}</Text>
+                    <Text style={{...styles.remainingTime}}>
+                      {children(remainingTime)}
+                    </Text>
+                  </View>
                 </Animated.Text>
               )}
             </CountdownCircleTimer>
@@ -166,16 +214,21 @@ const styles = StyleSheet.create({
   },
   box1: {
     flex: 0.4,
-    backgroundColor: 'red',
+    backgroundColor: '#946f51',
     transform: [{rotateZ: '180deg'}],
   },
   box2: {
     flex: 0.4,
-    backgroundColor: 'blue',
+    backgroundColor: '#F0D9B5',
   },
   remainingTime: {
     fontSize: 46,
-    color: '#fff',
+    color: '#0e0e0e',
+  },
+  timer: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
   },
 });
 
